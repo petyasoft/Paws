e6fff62bd08527dd868e29e532c0140406983a48
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-11-08 23:52:17 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,43 @@
+# api id, hash
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '' # API TOKEN get in @BotFather
+CHAT_ID = '' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [5, 1200]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+
+# проверять прокси
+CHECK_PROXY = True
+
+# реф код - то что идет после startapp=
+REF_CODE = 'DceesgPO' # ставьте здесь свой реф код
+
+# мини задержки
+MINI_SLEEP = [2,8]
+
+# задержка между квестами
+QUEST_SLEEP = [20,40]
+
+# задержка после цикла
+BIG_SLEEP = [3*60*60,8*60*60]
+
+hello ='''              _                               __  _        
+ _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
+| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
+| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
+| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
+|_|                |___/        
+
+'''
